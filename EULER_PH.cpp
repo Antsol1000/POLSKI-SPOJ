6552f69fac7a7d6e694c4a0fa5fc899e2ee4e74d
Antoni Solarski <antsol1000@gmail.com>
2019-08-29 13:06:11 +0200

    cleaner code in EULER_PH

--- EULER_PH.cpp
+++ EULER_PH.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
 using namespace std;
 
-long long phi(long long n) { // phi Euler  function
-    if (n <= 1) return n;
-    if (n == 2) return 1;
-    for (long long p = 2; p*p <= n; p++) {
-        if (n % p == 0) {
-            long long q = 1, qq = p;
-            n /= p;
-            while (n % p == 0) {
-                q = qq;
-                qq *= p;
-                n /= p;
+typedef long long int LL;
+
+LL eulerPhi(LL number) { // phi Euler  function
+
+    /* we calculate phi(n)
+    we use recursive function
+    phi is muliplicative so phi(n) = phi(a)phi(b)
+    where ab = n and gcd(a, b) = 1
+    we use formula phi(prime^alhpa) = prime^alpha - prime^(alpha-1) */
+
+    if (number <= 3) // if number is 2 or 3 phi(number) = 2
+        return 1;
+    for (LL prime = 2; prime*prime <= number; prime++) {
+        // for loop like in eratostenes
+        if (number % prime == 0) {
+            // if number is divisible by prime
+            // we caluculate max alpha such that
+            // number % prime^alpha == 0
+            LL primeAlpha1 = 1, primeAlpha = prime; // prime^alpha - 1, prime^alpha
+            number /= prime;
+            while (number % prime == 0) {
+                //till the n is divisible by prime
+                primeAlpha1 = primeAlpha;
+                primeAlpha *= prime;
+                number /= prime;
             }
-            return (qq - q) * phi(n);
+            return (primeAlpha - primeAlpha1) * eulerPhi(number);
         }
     }
-    return n-1;
+    return number - 1; // if number is prime phi(number) = number - 1
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
     int nTests;
     cin >> nTests;
     while (nTests--) {
         long long n;
         cin >> n;
-        cout << phi(n) << "\n";
+        cout << eulerPhi(n) << "\n";
     }
     return 0;
 }
