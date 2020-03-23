cff3b9c4ccdb48ac71b420015022b5252663630b
Antoni Solarski <antsol1000@gmail.com>
2020-03-23 12:38:35 +0100

    repaired

--- PP0602B.cpp
+++ PP0602B.cpp
@@ -1,41 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(0);
     int t; cin >> t;
     while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n][k];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < k; j++)
+                cin >> arr[i][j];
 
-
-        int n, k; cin >> n >> k;
-        int A[n][k];
-        int B[n*k];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < k; j++) {
-                cin >> A[i][j];
-                int p = j*n;
-                p += (j % 2 == 0)? i : n - 1 - i;
-                B[p] = A[i][j];
-            }
-        }
-
-        int temp = B[n*k - 1];
-        for (int p = 0; p < n*k; p++)
-            swap(B[p], temp);
-
-        for (int p = 0; p < n*k; p++) {
-            int j = p / n;
-            int i = (j % 2 == 0)? (p % n) : n - 1 - (p % n);
-            A[i][j] = B[p];
-        }
+        for (int i = 0; i < k - 1; i++)
+            swap(arr[0][i], arr[0][i+1]);
+        for (int i = 0; i < n - 1; i++)
+            swap(arr[i][k-1], arr[i+1][k-1]);
+        for (int i = k - 1; i > 0; i--)
+            swap(arr[n-1][i], arr[n-1][i-1]);
+        for (int i = n - 1; i > 1; i--)
+            swap(arr[i][0], arr[i-1][0]);
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < k; j++)
-                cout << A[i][j] << " ";
+                cout << arr[i][j] << " ";
             cout << endl;
         }
-
-
     }
-    return 0;
 }
